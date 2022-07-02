57af6f8abc6fed72fc5d8ff83bbf617293bc3f62
Nuchi <nadinajauch@gmail.com>
2022-07-02 15:36:41 -0300

    rama alternativa

--- listado_cheques.py
+++ listado_cheques.py
@@ -12,24 +12,30 @@ def comparacion_regex(expresion,txt):#funcion que sirve para saber si un texto m
     coincide= True if comparacion else False
     return coincide
 
-def validacion_parametro_opcional(parametro,posicion):
-        if(parametro.lower() == 'pendiente' or parametro.lower() == 'aprobado' or parametro.lower() == 'rechazado'):
-            print('se trata de un cheque')
-        elif(comparacion_regex(expresion_regex_fecha,parametro)):
-            print('es una fecha')   
+def comprobar_parametro(posicion,estado,fecha):
+        if(sys.argv[posicion].lower() == 'pendiente' or sys.argv[posicion].lower() == 'aprobado' or sys.argv[posicion].lower() == 'rechazado'):
+            estado = sys.argv[posicion]
+        elif(comparacion_regex(expresion_regex_fecha,sys.argv[posicion])):
+            fecha = sys.argv[posicion]
         else:
-            print(f'no se ingresó ningún parametro válido para el prametro n° {posicion}')
+            print(f'no se ingresó ningún valor válido para el prametro n° {posicion}')
+def validacion_parametro_opcional(estado,fecha):  
+    if (len(sys.argv)==6):
+        comprobar_parametro(5,estado,fecha)
+    elif (len(sys.argv)==7): 
+        comprobar_parametro(6,estado,fecha)
 
-def procesar_datos(dni,salida,tipo):
+        
+def procesar_datos(archivo,dni,salida,tipo,estado,fecha):
     lista=[] #creo lista donde voy a guardar como elemento cada row(fila) que me llega del archivo
     csv.register_dialect('dialectoCheques',delimiter=',',quoting=csv.QUOTE_ALL)#creo un dialecto en el cual digo que el delimitante de cada elemento es una coma
-    file=open('archivo.csv')#abro el archivo de nombre archivo tipo csv
-    csvFile=csv.DictReader(file,dialect='dialectoCheques') #creo un lector de diccionario para mi archivo, que tenga en cuenta el "dialecto" para separar con comas
+    archivo=open(f'{archivo}')#abro el archivo de nombre archivo 
+    csvFile=csv.DictReader(archivo,dialect='dialectoCheques') #creo un lector de diccionario para mi archivo, que tenga en cuenta el "dialecto" para separar con comas
 
-    for row in csvFile:  #para cada linea le agrego la linea correspondiente a mi lista 
-            if(row['DNI']==dni and (row['Tipo']).lower()==tipo):
-                lista.append(dict(row))
-    file.close()#cierro archivo
+    for row in csvFile:  #para cada linea le agrego la linea correspondiente a mi lista si cumple con los filtros
+        if(row['DNI']==dni and row['Tipo'].lower()==tipo and ((estado != None and row["Estado"] == estado) or estado == None)):
+            lista.append(dict(row))
+    archivo.close()
 
     if salida == "pantalla":
         print(lista)
@@ -45,29 +51,26 @@ def procesar_datos(dni,salida,tipo):
             print(f"Tipo de cheque: {cheque['Tipo']}")
             print(f"Estado de cheque:{ cheque['Estado']}")
     elif salida == "csv":
-        new_file=open(f'{dni}{datetime.timestamp(datetime.now())}.csv','w+')
-        # new_file.writelines('NroCheque,CodigoBanco,CodigoSucursal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado')
-        escribir=writer(new_file)
-        escribir.writerow(dict(lista).keys())
-        for cheque  in lista:
-            escribir.writerow(cheque['DNI'])
-            escribir.writerow(cheque['NroCheque'])
-            escribir.writerow(cheque['CodigoBanco'])
-        new_file.close()
-        #for cheque  in lista:
-                #escribir.writerow(cheque['NroCheque'])
+        nuevo_archivo=open(f'{dni}{datetime.timestamp(datetime.now())}.csv','w',newline='')
+        campos_header = ['NroCheque','CodigoBanco','CodigoSucursal','NumeroCuentaOrigen','NumeroCuentaDestino','Valor','FechaOrigen','FechaPago','DNI','Tipo','Estado']
+        writer = csv.DictWriter(nuevo_archivo, delimiter=",", fieldnames=campos_header)
+        writer.writeheader()
 
-        #            new_file.write(f"{cheque['DNI']},")
-        #            new_file.write(f"{cheque['NroCheque']}, ")
-        #            new_file.write(f"{cheque['CodigoBanco']}, ")
-        #            new_file.write(f"{cheque['NumeroCuentaOrigen']}, ")
-        #            new_file.write(f"{cheque['NumeroCuentaDestino']}, ")
-        #            new_file.write(f"{cheque['Valor']}, ")
-        #            new_file.write(f"{datetime.fromtimestamp(int(cheque['FechaOrigen']))}, ")
-        #            new_file.write(f"{datetime.fromtimestamp(int(cheque['FechaPago']))}, ")
-        #            new_file.write(f"{cheque['Tipo']}, ")
-        #            new_file.write(f"{cheque['Estado']}, ")
-                    
+        for cheque in lista:
+            writer.writerow({'NroCheque': cheque["NroCheque"],
+                            'CodigoBanco':cheque["CodigoBanco"],
+                            'CodigoSucursal':cheque["CodigoSucursal"],
+                            'NumeroCuentaOrigen':cheque["NumeroCuentaOrigen"],
+                            'NumeroCuentaDestino':cheque["NumeroCuentaDestino"], 
+                            'Valor':cheque["Valor"],
+                            'FechaOrigen':datetime.fromtimestamp(int(cheque["FechaOrigen"])),
+                            'FechaPago':datetime.fromtimestamp(int(cheque["FechaPago"])),
+                            'DNI':cheque["DNI"],
+                            'Tipo':cheque["Tipo"],
+                            'Estado':cheque["Estado"]})
+        
+        nuevo_archivo.close()
+
 if __name__=='__main__': #Entra en este if cuando apenas se ejecuta el codigo
     #sys.argv es un array donde se guaran los parametros ingresados en consola, (sys.argv[0]) si o si siempre debe ser el nombre del archivo py, por eso en lo que muestro no lo tengo en cuenta
     if (len(sys.argv) < 5):# Quiero minimo 4 parametros, la longitud es 5 porque tambien está el sys.argv[0]
@@ -77,20 +80,13 @@ if __name__=='__main__': #Entra en este if cuando apenas se ejecuta el codigo
     dni = sys.argv[2]
     salida = sys.argv[3]
     tipo_cheque = sys.argv[4]
+    estado = None
+    fecha = None
 
-    
-    if (len(sys.argv) ==5): #cuando se ingresan la cantidad de parametros obligatorios(4), 
-        print("Están bien los parámetros")
-        procesar_datos(dni,salida,tipo_cheque)
-    elif len(sys.argv)==6:#hay un quinto parametro, el ultimo parametro es opcional
-        parametro_opcional_1 = sys.argv[5]
-        validacion_parametro_opcional(parametro_opcional_1,5)
+    validacion_parametro_opcional(estado,fecha)
         
-    elif(len(sys.argv)==7):#hay un sexto parametro, los dos ultimos parametros son opcionales
-        parametro_opcional_1 = sys.argv[5]
-        parametro_opcional_2 = sys.argv[6]   
-        validacion_parametro_opcional(parametro_opcional_1,5)
-        validacion_parametro_opcional(parametro_opcional_2,6)
-        
-    elif(len(sys.argv)>7):
+    if (len(sys.argv)>7):
         print('Ingresaste una cantidad de parametros incorrecta, acordate que podes pasar hasta 6 parametros (4 obligatorios, 2 opcionales')
+
+
+    procesar_datos(archivo,dni,salida,tipo_cheque,estado,fecha)
